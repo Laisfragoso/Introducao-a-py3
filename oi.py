389af8a6739915fae545cc56572c73dd4f2a71b5
laisfgzz <fragosolais18@gmail.com>
2021-09-18 09:48:08 -0300

    primeiro commit

---
+++ oi.py
@@ -0,0 +1,5 @@
+Python 3.7.8 (tags/v3.7.8:4b47a5b6ba, Jun 28 2020, 08:53:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ('hello , world')
+hello , world
+>>> 
